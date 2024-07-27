ba61acefec7d7330ff04b7dbba22be6fac2011db
mahmoudmagdy1-1 <d4rkppl1@gmail.com>
2024-07-28 00:04:05 +0300

    local

---
+++ Binary Tree/Medium to Hard/4_v1.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cassert>
+using namespace std;
+// solution at line 45 && https://leetcode.com/problems/symmetric-tree/submissions/1335514874/
+struct TreeNode{
+	TreeNode* left{};
+	TreeNode* right{};
+	int val{};
+	TreeNode(int val) : val(val) {}
+};
+
+struct BinaryTree{
+	TreeNode* root{};
+	BinaryTree(int val) : root(new TreeNode(val)) {}
+	void _print_inorder(TreeNode* TreeNode){
+		if(!TreeNode) return;
+		_print_inorder(TreeNode->left);
+		cout << TreeNode->val << " ";
+		_print_inorder(TreeNode->right);
+	}
+	void add(vector<int> values, vector<char> direction){
+		TreeNode* temp = this->root;
+		for (int i = 0; i < values.size(); i++)
+		{
+			if(direction[i] == 'L'){
+				if(!temp->left)
+					temp->left = new TreeNode(values[i]);
+				temp = temp->left;
+			}
+			else{
+				if(!temp->right)
+					temp->right = new TreeNode(values[i]);
+				temp = temp->right;
+			}
+		}
+	}
+	void print_inorder(){
+		_print_inorder(root);
+		cout << endl;
+	}
+}; 
+class Solution {
+public:
+	bool eq(TreeNode* r1, TreeNode* r2){
+        if(!r1 && !r2) return 1;
+        if(!r1 || !r2 || r1->val != r2->val) return 0;
+        return eq(r1->left, r2->right) && eq(r1->right, r2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return eq(root->left, root->right);
+    }
+};
+
+
+int main() {
+	BinaryTree tree(1);
+	tree.add( { 2, 4, 7 }, { 'L', 'L', 'L' });
+	tree.add( { 2, 4, 8 }, { 'L', 'L', 'R' });
+	tree.add( { 2, 5, 9 }, { 'L', 'R', 'R' });
+	tree.add( { 3, 6, 10 }, { 'R', 'R', 'L' });
+
+	cout << Solution().isSymmetric(tree.root) << "\n";
+}
